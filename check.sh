4bdaedbaa04551ed528584d44e4c09fa86317cbe
Anders Borud <post@andersborud.com>
2025-10-30 07:45:37 +0100

    Add config parameter support

--- check.sh
+++ check.sh
@@ -1,22 +1,33 @@
 #!/bin/sh -l
 
+if [ $# -eq 0 ]
+  then
+    echo "Missing required filter pattern argument"
+    exit 1
+fi
+
+
 ERRORS=0
+filter_pattern=$1
+allowed_newlines=${2:-1}
 
 git config --global --add safe.directory /github/workspace
 
-for f in $(git ls-files '*.csv')
+cmd="git ls-files $filter_pattern"
+
+for f in $($cmd)
 do
   amount=$(grep -cv '[^[:blank:]]' $f)
-  if [[ $amount -gt 1 ]]; then
+  if [[ $amount -gt $allowed_newlines ]]; then
     total_lines=$(wc -l < $f | xargs)
     line=$((total_lines - amount))
-    echo "::error file=$f,line=$line,endLine=$total_lines::File has $amount extra trailing newlines at the end."
+    echo "::error file=$f,line=$line,endLine=$total_lines::File '$f' has $amount extra trailing newlines at the end."
     ERRORS=1
   fi
 done
 
 if [ $ERRORS -eq 1 ]; then
-  echo "Files above have more than one trailing newline at the end."
+  echo "Files above have more than $allowed_newlines trailing newline at the end."
   exit 1
 fi
 
